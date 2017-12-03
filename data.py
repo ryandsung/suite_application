f45664e16cbfa04b3127c64097a345a7b4bb2377
Pratyush T. Kapur <pkapur@hmc.edu>
2017-12-02 20:53:22 -0800

    adding folders and files

---
+++ data.py
@@ -0,0 +1,39 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Picture 1',
+            'body': 'Test body',
+            'author': 'Prat Kapur',
+            'date': '12-2-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Picture 2',
+            'body': 'Test body',
+            'author': 'Prat Kapur',
+            'date': '12-2-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Picture 3',
+            'body': 'Test body',
+            'author': 'Prat Kapur',
+            'date': '12-2-2017'
+        },
+        {
+            'id': 4,
+            'title': 'Picture 4',
+            'body': 'Test body',
+            'author': 'Prat Kapur',
+            'date': '12-2-2017'
+        },
+        {
+            'id': 5,
+            'title': 'Picture 5',
+            'body': 'Test body',
+            'author': 'Prat Kapur',
+            'date': '12-2-2017'
+        },   
+    ]
+    return articles
